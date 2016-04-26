e9ca160fd7a88abe10221a51e80c2801155a6868
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-04-26 22:32:03 +0900

    update install.bat

--- install.bat
+++ install.bat
@@ -1,17 +1,26 @@
 @echo off
 setlocal
 
-cd %~dp0
-git pull
+set DEST="%UserProfile%\.moe-config\"
 
-cd %HOME%
-
-mklink /J svl %~dp0\svl
-
-if "%AUTOPREP%" == "1" (
-    set MOE_MENUS="%~dp0\.moe-menus"
+if exist %DEST% (
+    cd %DEST%
+    git pull
 ) else (
-    set MOE_MENUS="%~dp0\.moe-menus-noautoprep"
+    git clone "https://github.com/philopon/.moe-config.git" %DEST%
+    cd %DEST%
+)
+
+cd %UserProfile%
+
+mklink /J svl %DEST%\svl
+
+if defined AUTOPREP (
+    set MOE_MENUS="%DEST%\.moe-menus"
+) else (
+    set MOE_MENUS="%DEST%\.moe-menus-noautoprep"
 )
 
 mklink /H .moe-menus %MOE_MENUS%
+
+set /p dummy="done!"
